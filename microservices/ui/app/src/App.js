3456b8b7c9393c3fb476de04af3ee93f968d671e
Truong Hoang Dung <checkraiser11@gmail.com>
2018-05-21 23:31:13 +0700

    Add test

--- microservices/ui/app/src/App.js
+++ microservices/ui/app/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Test
+          Test 1
         </p>
       </div>
     );
